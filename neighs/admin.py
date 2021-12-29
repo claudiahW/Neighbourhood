a40bb770688162ff35f132fc6fc2fed7e54beefb
claudiahW <murungiclaud@gmail.com>
2021-12-29 11:59:52 +0300

    register models in admin

--- neighs/admin.py
+++ neighs/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import NeighborHood,Profile,Location
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(NeighborHood)
+admin.site.register(Location)
