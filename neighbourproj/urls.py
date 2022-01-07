06a580f473c8e9d9f3f8d24db71861b959735a89
claudiahW <murungiclaud@gmail.com>
2022-01-08 00:59:12 +0300

    add logout url

--- neighbourproj/urls.py
+++ neighbourproj/urls.py
@@ -25,11 +25,11 @@ from django_registration.backends.one_step.views import RegistrationView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('neighs.urls')),
-    path('accounts/register/', RegistrationView.as_view(success_url='/create_profile'),
+    path('accounts/register/', RegistrationView.as_view(success_url='/'),
         name='django_registration_register'),
-    # path('accounts/', include('django_registration.backends.one_step.urls')),
-    # path('accounts/', include('django.contrib.auth.urls')),
-    # path('logout/', views.logout_then_login, name='logout'), 
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('logout/', views.logout_then_login, name='logout'), 
     
     
 ] 
